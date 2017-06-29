11dd4d9384891958375fab212d9d6aea7d35a502
srikanth121 <s_tadise@live.concordia.ca>
2017-06-28 17:29:53 -0700

    testing

---
+++ cLearning/cLearning/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cLearning
+//
+//  Created by Srikanth Tadisetty on 6/28/17.
+//  Copyright © 2017 Srikanth Tadisetty. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0; //
+}
